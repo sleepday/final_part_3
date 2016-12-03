f257b56a685bdf8ffbe808466183b42ff1b404b8
chinnawat sinnarai <57160171@go.buu.ac.th>
2016-12-03 15:00:56 +0700

    new

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/kingsong1'
+  get 'kingsong1'
 
-  get 'static_pages/kingsong2'
+  get 'kingsong2'
 
 root 'application#hello'
   # The priority is based upon order of creation: first created -> highest priority.
