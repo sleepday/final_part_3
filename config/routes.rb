d349fc797f210632a2e5209a689143e1b74e87c2
chinnawat sinnarai <57160171@go.buu.ac.th>
2016-12-03 15:30:35 +0700

    Signed-off-by: chinnawat sinnarai <57160171@go.buu.ac.th>

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get '/kingsong1'  to: 'static_pages#kingsong1'
-  get '/kingsong2'  to: 'static_pages#kingsong2'
+  get '/kingsong1' , to: 'static_pages#kingsong1'
+  get '/kingsong2' , to: 'static_pages#kingsong2'
 
 root 'application#hello'
   # The priority is based upon order of creation: first created -> highest priority.
